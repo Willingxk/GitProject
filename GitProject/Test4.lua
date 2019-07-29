e4c50ad445b75ff31ee94b39eb4920ab3949ea9c
Willingxk <m13682706284_1@163.com>
2019-07-29 17:15:19 +0800

    [Test4测试]

---
+++ GitProject/Test4.lua
@@ -0,0 +1,15 @@
+local uiList={}
+
+local Test3={}
+
+local tool=require("Tool.lua")
+
+local title= tool.CreateTitle("ĞÕÃû")
+
+uiList["ĞÕÃû"]=title
+
+uiList["ÃÜÂë"]=tool:GetPSW()
+
+
+return uiList
+
